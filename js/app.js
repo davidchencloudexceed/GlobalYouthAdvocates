5beb1eb9c5a555663f602e76b881197f8d34afa0
davidchencloudexceed <david.chen@cloudexceed.com>
2015-07-31 09:26:40 -0700

    add angular

---
+++ js/app.js
@@ -0,0 +1,47 @@
+﻿(function () {
+    "use strict";
+    var app = angular.module("productManagement",
+        [
+            //"common.services",
+            "ui.router",
+            "ui.mask",
+            "ui.bootstrap"
+            //, "productResourceMock"
+        ]);
+
+    app.config([
+            "$stateProvider",
+            "$urlRouterProvider",
+
+            function ($stateProvider, $urlRouterProvider, $http) {
+                $urlRouterProvider.otherwise("/");
+
+                $stateProvider
+                    .state("home", {
+                        url: "/",
+                        templateUrl: "/home.html"
+                    })
+                .state("about", {
+                    url: "/about",
+                    templateUrl: "/about.html"
+                })
+                .state("contact", {
+                    url: "/contact",
+                    templateUrl: "/contact.html"
+                })
+                .state("event", {
+                    url: "/event",
+                    templateUrl: "/event.html"
+                })
+                .state("faq", {
+                    url: "/faq",
+                    templateUrl: "/faq.html"
+                })
+                .state("donate", {
+                    url: "/donate",
+                    templateUrl: "/donate.html"
+                })
+            }]
+    );
+
+}());
